ce812a6a78ccc933d7060e2c6d5a4256ec96574e
messias. ernandes <messiasfernandes@gmail.com>
2023-12-07 17:46:35 -0300

    instalação primeng e bibliotecas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListagemProdutosComponent } from './listadeprodutos/listagem-produtos/listagem-produtos.component';
 
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import localePt from '@angular/common/locales/pt';
+import { PrimengModule } from './shared/primeng/primeng.module';
+registerLocaleData(ptBr, localePt);
+
+registerLocaleData(localePt, 'pt-BR');
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListagemProdutosComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/listadeprodutos/listagem-produtos/listagem-produtos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem-produtos',
+  templateUrl: './listagem-produtos.component.html',
+  styleUrls: ['./listagem-produtos.component.css']
+})
+export class ListagemProdutosComponent {
+
+}
